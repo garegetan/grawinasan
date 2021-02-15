55a85da366c900a546d6799ed526901ba0b309c0
garegetan <79066977+garegetan@users.noreply.github.com>
2021-02-15 09:52:51 +0700

    Create baganin.sh

---
+++ baganin.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+
+toilet -f big -F gay FAUZAN CK
+echo
+sleep 2
+clear
+
+echo "
+   =======================================
+    SELAMAT DATANG DI TOOLS FAUZAN PRATAMA
+   =======================================
+" | lolcat
+sleep 2
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+toilet -f big -F PASSWORD
+echo $green"+=============================+"
+echo $green"    SILAHKAN MASUKAN PASSWORD 
+echo $green"            PW NYA : fauzanpratama
+echo $green"+=============================+"
+
+name="fauzanpratama"
+while [ "$adin" != "$name" ]
+do
+  read -p "MASUKAN PASSWORD NGAB:)" adin
+done
+  echo "OK PASSWORD ANDA BENAR..."
+  sleep 2
+  clear
+  
+  blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay  WEL
+toilet -f big -F gay COME
+
+echo $yellow"   =================================
+echo $yellow"        CREATOR : FAUZAN PRATAMA"
+echo $yellow"        KONTAK  : 081276754581"
+echo $yellow"   =================================
+echo
+echo $red" [ 01 ] BUG TSEL WHATSAPP"
+echo $red" [ 02 ] BUG TSEL INSTAGRAM"
+echo $red" [ 03 ] BUG TSEL YOUTUBE"
+echo $red" [ 04 ] BUG  AXIS GAME"
+echo $white" PILIH MENU DI ATAS CONTOH INPUT (01)
+read -p " [ PILIH MENUNYA ]>>>)" pilih ;
+if [ $pilih = "01" ]
+then
+clear
+toilet -f big -F gay Bug wa
+echo $green"Loading...."
+sleep 2
+echo "================"
+echo " www.whatsapp.com"
+echo "================="
+sleep 2
+echo $white"Menggunakan mode ssl"
+fi
+if [ $pilih = "02" ]
+then
+clear
+toilet -f big -F gay Bug instagram
+sleep 2
+echo $green"Loading...."
+echo "=============="
+echo "m.instagram.com"
+echo "=============="
+sleep 2
+echo $white"Menggunakan mode ssl"
+fi
+if [ $pilih = "03" ]
+then
+clear
+toilet -f big -F gay Bug yt
+echo $green"Loading...."
+sleep 2
+echo "==============="
+echo "www.youtube.com
+echo "==============="
+sleep 2
+echo "Menggunakan mode ssl"
+fi
